308bd0601dca94efccd44cc856410a88be60b968
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-04-29 21:09:06 -0700

    chore: Update backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,5 +3,8 @@
 rm -rf var/cache/*
 tar --exclude='var/lib/libvirt/images' \
     --exclude='usr/src' \
+    --exclude='tmp/*' \
+    --exclude='var/db/repos' \
+    --exclude='var/db/pkg' \
     -cvf $(cat etc/hostname)-rootfs-$(date +%Y%m%d).tar \
     bin boot dev etc lib lib64 media mnt opt proc root run sbin swapfile sys tmp usr var

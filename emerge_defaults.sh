5ee3272d32c2541d2c040994a48e792e078784ea
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-11-12 14:15:41 -0800

    feat: Use array for package definitions

--- emerge_defaults.sh
+++ emerge_defaults.sh
@@ -1,27 +1,42 @@
 #! /usr/bin/env bash
 
-emerge --ask \
-    app-arch/unzip \
-    app-arch/zip \
-    app-editors/vim \
-    app-admin/sudo \
-    app-misc/tmux \
-    dev-vcs/git \
-    app-shells/bash-completion \
-    sys-process/htop \
-    net-analyzer/netcat \
-    net-analyzer/nmap \
-    app-text/dos2unix \
-    app-portage/gentoolkit \
-    media-fonts/noto-emoji \
-    media-fonts/noto \
-    media-fonts/fontawesome \
-    media-fonts/liberation-fonts \
-    media-fonts/dejavu \
-    dev-util/ctags
+declare base=(
+    # Network sharing
+    "net-fs/cifs-utils"
+    # Archive tools
+    "app-arch/unzip"
+    "app-arch/zip"
+    # Developer tools
+    "app-editors/vim"
+    "dev-vcs/git"
+    "app-text/dos2unix"
+    "dev-util/ctags" # ctags needed for jump to definition in vim
     # dev-lang/go
+    # Networking
+    "net-analyzer/netcat"
+    "net-analyzer/arp-scan"
+    "net-analyzer/nmap"
+    # System/Admin tools
+    "app-misc/tmux"
+    "sys-process/htop"
+    "sys-apps/pciutils"
+    "sys-apps/usbutils"
+    "app-portage/gentoolkit" # 'equery uses <package>' to see available use flags
+    # Bash tools
+    "app-admin/sudo"
+    "app-shells/bash-completion"
+)
+
+declare desktop=(
+    # Fonts
+    "media-fonts/noto-emoji"
+    "media-fonts/noto"
+    "media-fonts/fontawesome"
+    "media-fonts/liberation-fonts"
+    "media-fonts/dejavu"
+)
+
+emerge --ask ${base[@]}
 
 emerge --oneshot --ask \
     app-portage/cpuid2cpuflags
-
-
